280e27cdf7d9d4dc347ee181cfe1bc39fd3d6144
mikedido <mahdigueffaz@gmail.com>
2022-03-22 11:17:05 +0100

    Refacto index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import Survey from './pages/Survey';
+import Header from './components/Header/index';
+import Footer from './components/Footer/index';
+import Freelances from './pages/Freelances/index';
+import Results from './pages/results/index';
+import Error from './components/Error/index';
+import GlobalStyle from './utils/style/GlobalStyle';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider, SurveyProvider } from './utils/context'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ThemeProvider>
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/survey/:questionNumber' element={<Survey />} />
+            <Route path='/freelances' element={<Freelances />} />
+            <Route path='/results' element={<Results />} />
+            <Route path='*' element={<Error />} />
+          </Routes>
+          <Footer />
+        </SurveyProvider>
+      </ThemeProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
