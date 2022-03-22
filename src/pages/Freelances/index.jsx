2b2468c2ca54d2f9053859396c2a0ccfe013c9d2
mikedido <mahdigueffaz@gmail.com>
2022-03-22 13:00:42 +0100

    UseFetch to get data from the API

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,47 +1,77 @@
 import DefaultPicture from '../../assets/profile.png'
-import Card from '../../components/Card/index';
-import Styled from 'styled-components';
+import Card from '../../components/Card/index'
+import Styled from 'styled-components'
+import { Loader } from '../../utils/Loader'
+import colors from '../../utils/style/colors'
+import { useFetch, useTheme } from '../../utils/hooks'
 
-const CardContainer = Styled.div`
-    display: grid;
-    gap: 24px;
-    grid-template-rows: 350px 350px;
-    grid-template-columns: repeat(2, 1fr);
-`;
+
+const CardsContainer = Styled.div`
+  display: grid;
+  gap: 24px;
+  grid-template-rows: 350px 350px;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  justify-items: center;
+`
  
-const freelanceProfiles = [
-    {
-        name: 'Jane Doe',
-        jobTitle: 'Devops',
-        picture: DefaultPicture,
-    },
-    {
-        name: 'John Doe',
-        jobTitle: 'Developpeur frontend',
-        picture: DefaultPicture,
-    },
-    {
-        name: 'Jeanne Biche',
-        jobTitle: 'Développeuse Fullstack',
-        picture: DefaultPicture,
-    },
-]
+const LoaderWrapper = Styled.div`
+  display: flex;
+  justify-content: center;
+`
 
+
+const PageTitle = Styled.h1`
+  font-size: 30px;
+  text-align: center;
+  padding-bottom: 30px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+`
+
+const PageSubtitle = Styled.h2`
+  font-size: 20px;
+  color: ${colors.secondary};
+  font-weight: 300;
+  text-align: center;
+  padding-bottom: 30px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+`
 
 function Freelances() {
+
+    const { theme } = useTheme();
+    const { data, isLoading, error } = useFetch(`http://localhost:8000/freelances`);
+
+    if (error) {
+        return <span>Il y a un problème</span>
+    }
+
+    const freelancersList = data?.freelancersList
+
+    console.log(data)
+
     return (
         <div>
-            <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
-            <CardContainer>
-            {freelanceProfiles.map((profile, index) => (
-                <Card
-                    key={`${profile.name}-${index}`}
-                    label={profile.jobTitle}
-                    picture={profile.picture}
-                    title={profile.name}
-                />
-            ))}
-            </CardContainer>
+            <PageTitle theme={theme}>Trouvez votre prestataire</PageTitle>
+            <PageSubtitle theme={theme}>
+                Chez Shiny nous réunissons les meilleurs profils pour vous.
+            </PageSubtitle>
+            {isLoading ?
+            (<LoaderWrapper>
+                <Loader />
+            </LoaderWrapper>) : 
+            (
+                <CardsContainer>
+                    {freelancersList?.map((profile, index) => (
+                        <Card
+                            key={`${profile.name}-${index}`}
+                            label={profile.job}
+                            picture={profile.picture}
+                            title={profile.name}
+                        />
+                    ))}
+                </CardsContainer>
+            )}
         </div>
     )
 }
